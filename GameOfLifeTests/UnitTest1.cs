c46f0b1d1521c4c692e68cc1e05044430b1f0689
hatelove <jay.hatelove@gmail.com>
2017-07-12 15:07:44 +0800

    a failed test: 2 alive cells around, the start position should be still alive.

--- GameOfLifeTests/UnitTest1.cs
+++ GameOfLifeTests/UnitTest1.cs
@@ -2,6 +2,110 @@
 
 namespace GameOfLifeTests
 {
+    public class Dimension
+    {
+        public Dimension _next;
+        public Dimension _previous;
+        public Dimension(Dimension previous, Dimension next)
+        {
+            this._previous = previous;
+            this._next = next;
+        }
+
+        public Dimension next()
+        {
+            if (this._next == null)
+                this._next = new Dimension(this, null);
+            return this._next;
+        }
+
+        public Dimension previous()
+        {
+            if (this._previous == null)
+                this._previous = new Dimension(null, this);
+            return this._previous;
+        }
+    }
+
+    public class Position
+    {
+        private Dimension x;
+        private Dimension y;
+
+        public Position(Dimension x, Dimension y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Position down()
+        {
+            return new Position(this.x, this.y.next());
+        }
+
+        public Position downleft()
+        {
+            return new Position(this.x.next(), this.y.next());
+        }
+
+        public Position downright()
+        {
+            return new Position(this.x.previous(), this.y.next());
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            return x.Equals(((Position)obj).x) && y.Equals(((Position)obj).y);
+        }
+        public Position left()
+        {
+            return new Position(x.next(), this.y);
+        }
+
+        public Position right()
+        {
+            return new Position(x.previous(), this.y);
+        }
+
+        public Position up()
+        {
+            return new Position(this.x, this.y.previous());
+        }
+        public Position upleft()
+        {
+            return new Position(this.x.next(), this.y.previous());
+        }
+
+        public Position upright()
+        {
+            return new Position(this.x.previous(), this.y.previous());
+        }
+    }
+
+    public class PositionList
+    {
+        private PositionList _next;
+        private Position _p;
+        public void add(Position position)
+        {
+            if (this._p != null)
+            {
+                this._next = new PositionList();
+                this._next.add(position);
+            }
+            else
+                this._p = position;
+        }
+        public bool include(Position position)
+        {
+            if (this._p == null) return false;
+            return (this._p.Equals(position) || this._next.include(position));
+        }
+
+    }
+
     [TestClass]
     public class UnitTest1
     {
@@ -10,9 +114,26 @@ namespace GameOfLifeTests
         //ctrl+R, A => run all tests
 
         [TestMethod]
-        public void SamePositionEqualToEachOther()
+        public void AnAlivePositionWithNoAliveNeighbourShouldBeDeadInNextGenerate()
         {
-            Assert.AreEqual(this._startPosition, this._startPosition);
+            this._world.alive(this._startPosition);
+            Assert.IsTrue(this._world.nextGeneration().isDead(this._startPosition));
+        }
+
+        [TestMethod]
+        public void AnAlivePositionWithTwoAliveNeighboursShouldSurviveInNextGenerate()
+        {
+            this._world.alive(this._startPosition);
+            this._world.alive(this._startPosition.left());
+            this._world.alive(this._startPosition.right());
+            Assert.IsFalse(this._world.nextGeneration().isDead(this._startPosition));
+        }
+
+        [TestMethod]
+        public void LeftIsSameAsLeftAndRightIsRight()
+        {
+            Assert.AreEqual(this._startPosition.left(), this._startPosition.left());
+            Assert.AreEqual(this._startPosition.right(), this._startPosition.right());
         }
 
         [TestMethod]
@@ -34,10 +155,30 @@ namespace GameOfLifeTests
         }
 
         [TestMethod]
-        public void LeftIsSameAsLeftAndRightIsRight()
+        public void SamePositionEqualToEachOther()
         {
-            Assert.AreEqual(this._startPosition.left(), this._startPosition.left());
-            Assert.AreEqual(this._startPosition.right(), this._startPosition.right());
+            Assert.AreEqual(this._startPosition, this._startPosition);
+        }
+        [TestInitialize]
+        public void SetUp()
+        {
+            _world = new World();
+            _startPosition = _world.startPosition();
+        }
+
+        [TestMethod]
+        public void shouldBeAliveIfSetAlive()
+        {
+            this._world.alive(this._startPosition);
+            this._world.alive(this._startPosition.left());
+            Assert.IsFalse(this._world.isDead(this._startPosition));
+            Assert.IsFalse(this._world.isDead(this._startPosition.left()));
+        }
+
+        [TestMethod]
+        public void shouldBeDeadIfNotSetAlive()
+        {
+            Assert.IsTrue(this._world.isDead(this._startPosition));
         }
 
         [TestMethod]
@@ -55,162 +196,10 @@ namespace GameOfLifeTests
             Assert.AreEqual(this._startPosition.downright().up().left(), this._startPosition);
             Assert.AreEqual(this._startPosition.downleft().up().right(), this._startPosition);
         }
-
-        [TestMethod]
-        public void shouldBeDeadIfNotSetAlive()
-        {
-            Assert.IsTrue(this._world.isDead(this._startPosition));
-        }
-
-        [TestMethod]
-        public void shouldBeAliveIfSetAlive()
-        {
-            this._world.alive(this._startPosition);
-            this._world.alive(this._startPosition.left());
-            Assert.IsFalse(this._world.isDead(this._startPosition));
-            Assert.IsFalse(this._world.isDead(this._startPosition.left()));
-        }
-
-        
-        [TestMethod]
-        public void AnAlivePositionWithNoAliveNeighbourShouldBeDeadInNextGenerate()
-        {
-            this._world.alive(this._startPosition);
-            Assert.IsTrue(this._world.nextGeneration().isDead(this._startPosition));
-        }
-
-        [TestMethod]
-        public void AnAlivePositionWithTwoAliveNeighboursShouldSurviveInNextGenerate()
-        {
-            this._world.alive(this._startPosition);
-            this._world.alive(this._startPosition.left());
-            this._world.alive(this._startPosition.right());
-            Assert.IsFalse(this._world.nextGeneration().isDead(this._startPosition));
-        }
-
-
-        [TestInitialize]
-        public void SetUp()
-        {
-            _world = new World();
-            _startPosition = _world.startPosition();
-        }
-    }
-
-    public class Dimension
-    {
-        public Dimension _previous;
-        public Dimension _next;
-
-        public Dimension(Dimension previous, Dimension next)
-        {
-            this._previous = previous;
-            this._next = next;
-        }
-
-        public Dimension previous()
-        {
-            if (this._previous == null)
-                this._previous = new Dimension(null, this);
-            return this._previous;
-        }
-
-        public Dimension next()
-        {
-            if (this._next == null)
-                this._next = new Dimension(this, null);
-            return this._next;
-        }
-    }
-
-    public class Position
-    {
-        private Dimension x;
-        private Dimension y;
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null)
-                return false;
-            return x.Equals(((Position)obj).x) && y.Equals(((Position)obj).y);
-        }
-
-        public Position(Dimension x, Dimension y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-        public Position left()
-        {
-            return new Position(x.next(), this.y);
-        }
-
-        public Position right()
-        {
-            return new Position(x.previous(), this.y);
-        }
-
-        public Position up()
-        {
-            return new Position(this.x, this.y.previous());
-        }
-
-        public Position down()
-        {
-            return new Position(this.x, this.y.next());
-        }
-
-        public Position upright()
-        {
-            return new Position(this.x.previous(), this.y.previous());
-        }
-
-        public Position upleft()
-        {
-            return new Position(this.x.next(), this.y.previous());
-        }
-
-        public Position downright()
-        {
-            return new Position(this.x.previous(), this.y.next());
-        }
-
-        public Position downleft()
-        {
-            return new Position(this.x.next(), this.y.next());
-        }
-    }
-
-    public class PositionList
-    {
-        private Position _p;
-        private PositionList _next;
-        public void add(Position position)
-        {
-            if (this._p != null)
-            {
-                this._next = new PositionList();
-                this._next.add(position);
-            }
-            else
-                this._p = position;
-        }
-        public bool include(Position position)
-        {
-            if (this._p == null) return false;
-            return (this._p.Equals(position) || this._next.include(position));
-        }
-
     }
     public class World
     {
         private PositionList lives = new PositionList();
-        public Position startPosition()
-        {
-            return new Position(new Dimension(null, null), new Dimension(null, null));
-        }
-
         public void alive(Position position)
         {
             lives.add(position);
@@ -224,6 +213,11 @@ namespace GameOfLifeTests
         public World nextGeneration()
         {
             return new World();
+        }
+
+        public Position startPosition()
+        {
+            return new Position(new Dimension(null, null), new Dimension(null, null));
         }
     }
 }
